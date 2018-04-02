2b4c84d904a5ac12946617b15e181402fc24f0fe
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2018-04-02 15:57:52 +0530

    changes after finalizing endpoint naming convention.

--- src/routes.js
+++ src/routes.js
@@ -1,40 +1,16 @@
 'use strict';
 
-const config = require('config');
-
-const routes = {};
-
-const oldkeyEventType = '/email/templates/eventType/:name'
-routes[oldkeyEventType] = {
-  get: {
-    controller: 'TemplateController',
-    method: 'eventTypes',
-  }
+module.exports = {
+  '/templates/:name': {
+    get: {
+      controller: 'TemplateController',
+      method: 'eventTypes',
+    },
+  },
+  '/health': {
+    get: {
+      controller: 'HealthController',
+      method: 'health',
+    },
+  },
 };
-
-const oldkeyHealthCheck = '/email/health'
-routes[oldkeyHealthCheck] = {
-  get: {
-    controller: 'HealthController',
-    method: 'health',
-  }
-};
-
-const keyEventType = `/${config.API_VERSION}/${config.API_CONTEXT_PATH}/templates/eventType/:name`;
-routes[keyEventType] = {
-  get: {
-    controller: 'TemplateController',
-    method: 'eventTypes',
-  }
-};
-
-const keyHealthCheck = `/${config.API_VERSION}/${config.API_CONTEXT_PATH}/health`;
-routes[keyHealthCheck] = {
-  get: {
-    controller: 'HealthController',
-    method: 'health',
-  }
-};
-
-
-module.exports = routes;
