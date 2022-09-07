343eb296ae7856b106574227e92fa0edb2dc5df9
Hamid Tavakoli <hamid@seodapop.com>
2022-09-07 11:26:12 -0700

    Move the email msg to if statement

--- connect/service.js
+++ connect/service.js
@@ -12,30 +12,42 @@ sgMail.setApiKey(config.SENDGRID_API_KEY);
 const sendEmail = (templateId, message) => { // send email
 
   let msg = {}
-  msg.from = message.from ? message.from : config.EMAIL_FROM;
-  msg.replyTo = message.replyTo ? message.replyTo : config.EMAIL_FROM;
-  msg.categories = message.categories ? message.categories : [];
-  msg.to = message.recipients;
-  msg.cc = message.cc ? message.cc : [];
-  msg.bcc = message.bcc ? message.bcc : [];
-  msg.templateId = templateId;
-  const sendAt = message.sendAt ? message.sendAt : undefined;
-  const personalizations = message.personalizations ? message.personalizations : null
+  const from = message.from ? message.from : config.EMAIL_FROM;
+  const replyTo = message.replyTo ? message.replyTo : config.EMAIL_FROM;
   const substitutions = message.data;
+  const categories = message.categories ? message.categories : [];
+  const to = message.recipients;
+  const cc = message.cc ? message.cc : [];
+  const bcc = message.bcc ? message.bcc : [];
+  const sendAt = message.sendAt ? message.sendAt : null;
+  const personalizations = message.personalizations ? message.personalizations : null
   const attachments = message.attachments ? message.attachments : [];
-
 
   if (message.version && message.version == "v3") {
     msg = {
+      to,
+      templateId,
       dynamicTemplateData: substitutions,
-      attachments,
       personalizations,
+      from,
+      replyTo,
+      categories,
+      cc,
+      bcc,
+      attachments,
       sendAt
     };
   } else {
     msg = {
+      to,
+      templateId,
       substitutions,
       substitutionWrappers: ['{{', '}}'],
+      from,
+      replyTo,
+      categories,
+      cc,
+      bcc,
     };
   }
   return sgMail.send(msg)
